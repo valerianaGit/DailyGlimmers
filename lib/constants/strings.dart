bf18d2f2451992c297f93a742c2fdf34466c3723
vdr <valeria.do@gmail.com>
2023-10-19 09:10:40 -0500

    initial commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,1 @@
+const kAppTitle = 'Daily Glimmers';
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'constants/strings.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: kAppTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePageScreen(),
+    );
+  }
+}
+
